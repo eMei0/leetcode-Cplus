d0116fd3685edd3adc63b7c7ae3f9d48862ad459
eMei <sylvainhyc@gmail.com>
2025-03-04 19:58:29 +0800

    重构 35.searchInsert.cpp：使用迭代二分查找替代递归实现

--- binarySearch/35.searchInsert.cpp
+++ binarySearch/35.searchInsert.cpp
@@ -8,15 +8,19 @@ using namespace std;
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        return find(nums, target, 0, nums.size() - 1);
-    }
-
-    int find(vector<int>& nums, int target, int startPos, int endPos) {
-        if (startPos > endPos) return startPos;
+        int left = 0;
+        int right = nums.size() - 1;
         
-        int midPos = (startPos + endPos) >> 1;
-        if (nums[midPos] == target) return midPos;
-        if (nums[midPos] > target) return find(nums, target, startPos, midPos - 1);
-        else return find(nums, target, midPos + 1, endPos);
+        while (left <= right) { // 使用巡检，减少递归栈的使用
+            int mid = left + ((right - left) >> 1);  // 使用位运算优化，同时防止溢出
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
     }
 };
