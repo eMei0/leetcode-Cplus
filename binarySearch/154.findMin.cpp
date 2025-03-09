2fb63d5cdb5942acfbde99236a85d1fef6a3c378
eMei <sylvainhyc@gmail.com>
2025-03-09 17:24:47 +0800

    添加 154.findMin.cpp：在包含重复元素的旋转排序数组中使用二分查找寻找最小值

---
+++ binarySearch/154.findMin.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <list>
+
+using namespace std;
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1;
+
+        while (left < right)
+        {
+            if (nums[left] < nums[right]) return nums[left];    // 顺序数组
+            int mid = left + ((right - left) >> 1);
+
+            // 无法判断
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) 
+            {
+                left++;
+                right--;
+            }
+            else if (nums[left] <= nums[mid]) left = mid + 1; 
+            else right = mid;
+        }
+
+        return nums[left];
+    }
+};
